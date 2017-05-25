f928d628c09e253568f1ea9ee34348f700cbdbde
jbrown <jbrown@JAMES-DP250.datapak.local>
2017-05-25 13:02:26 -0400

    HEL-3: Jenkins setup
    Task-Url: http://172.16.150.32:8001/browse/HEL-3

---
+++ Hello-World3/src/main/java/com/datapak/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.datapak.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(getMessage());
+	}
+
+	public static String getMessage() {
+		// TODO Auto-generated method stub
+		return "Hello DataPak!";
+	}
+
+}
---
+++ Hello-World3/src/test/java/com/datapak/helloworld/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.datapak.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testgetMessage() {
+		
+		
+		assertEquals("Hello DataPak!", HelloWorld.getMessage());
+	}
+
+}
